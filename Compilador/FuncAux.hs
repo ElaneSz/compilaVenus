3352d8d3e477f410e9e79005ade2ae76fc7bd9e9
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2026-05-15 13:09:04 +0000

    Atualizacoes | FuncAux.hs

--- Compilador/FuncAux.hs
+++ Compilador/FuncAux.hs
@@ -7,5 +7,5 @@ geraIdFuncao (x :->: (a, b)) = x
 
 -- Pega o retorno do ListaFuncoes e transforma no tipo que o contrutor Prog aceita
 geraFuncaoCompleta :: [(Funcao,([Var], Bloco))] -> [(Id,[Var],Bloco)]
-geraFuncaoCompleta (((x :->: (a, b)), (vars, bloco)) : xs) = (x, vars, bloco) : geraFuncaoCompleta xs
+geraFuncaoCompleta (((x :->: (varsFunc, b)), (varsBloco, bloco)) : xs) = (x, varsFunc ++ varsBloco, bloco) : geraFuncaoCompleta xs
 geraFuncaoCompleta [] = []
