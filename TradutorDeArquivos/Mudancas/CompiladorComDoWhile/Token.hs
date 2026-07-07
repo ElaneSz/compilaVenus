f3eeebd681c715a9ef4670cbfc0da15eb5817db3
Matheus <matheus.eduardo.g.santana@gmail.com>
2026-07-07 08:39:58 -0300

    Compilador com Do While

---
+++ TradutorDeArquivos/Mudancas/CompiladorComDoWhile/Token.hs
@@ -0,0 +1,23 @@
+module Token where
+
+data Token
+  -- Palavras reservadas
+  = TKint | TKdouble | TKstring | TKvoid
+  | TKif | TKelse | TKwhile | TKprint | TKreturn
+  | TKread | TKdo
+  -- Operadores aritméticos
+  | TKmais | TKmenos | TKmult | TKdiv
+  -- Operadores relacionais
+  | TKlt | TKgt | TKle | TKge | TKeq | TKdif -- < > <= >= == /=
+  -- Operadores lógicos
+  | TKand | TKor | TKnot
+  -- Atribuição e pontuação
+  | TKatrib | TKabreP | TKfechaP | TKabreC | TKfechaC
+  | TKvirgula | TKponto_e_virgula
+  -- Literais e identificador
+  | TKid String
+  | TKint_lit Int
+  | TKdouble_lit Double
+  | TKstring_lit String
+  | TKEOF -- fim do arquivo
+  deriving (Show, Eq)
